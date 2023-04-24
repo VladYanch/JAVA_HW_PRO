6a10ad71af7dc6a7ea80d7529f97c4a006ccbc74
Vladyslav Yanchenko <vyanch72@gmail.com>
2023-04-24 18:16:13 +0200

    DB Language

---
+++ JH_0423/base/CountryLanguage.java
@@ -0,0 +1,18 @@
+package lesson20230419.base;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CountryLanguage {
+
+    private String countryCode;
+
+    private String language;
+
+    private String isOfficial;
+
+    private double percentage;
+
+}
