dff0f4c7cf1d8ee96213a2e7c74c9f8a4649f96c
Vladyslav Yanchenko <vyanch72@gmail.com>
2023-08-29 22:03:15 +0200

    HW 0826 elev unlim stage

--- JH_0826/src/Main.java
+++ JH_0826/src/Main.java
@@ -1,25 +1,56 @@
-import java.util.Random;
-import java.util.Scanner;
-
+import java.util.*;
 public class Main {
+    public static int request = 0;
+    public static int distMin = Integer.MAX_VALUE;
     public static void main(String[] args) {
+        Map<String,Integer> map = new LinkedHashMap<>();
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int floors = 2;
-        while (floors > 1) {
-            System.out.println("Введите количество этажей: ");
-            floors = scanner.nextInt();
-            int elev1 = random.nextInt(1, floors);
-            int elev2 = random.nextInt(1, floors);
-            int request = random.nextInt(1, floors);
 
-            if (Math.abs(request - elev1) <= Math.abs(request - elev2)) {
-                System.out.println("На вызов с " + request + " этажа едет лифт 1 c "
-                        + elev1 + " этажа, т.к. лифт 2 стоит на " + elev2 + " этаже");
-            } else {
-                System.out.println("На вызов с " + request + " этажа едет лифт 2 c "
-                        + elev2 + " этажа, т.к. лифт 1 стоит на " + elev1 + " этаже");
-            }
+        System.out.println("Введите количество этажей >=2: ");
+        int floors = scanner.nextInt();
+//        int floors = 4;
+
+        System.out.println("Введите количество лифтов >=2: ");
+        int elevators = scanner.nextInt();
+//        int elevators = 4;
+
+        Character elev = 'A';
+        for (int i = 0; i < elevators; i++) {
+            map.put(elev.toString(),random.nextInt(1, floors));
+            elev ++;
+        }
+
+        request = random.nextInt(1, floors);
+
+        System.out.println("Лифты расположены: ");
+        System.out.println(map);
+        System.out.println("Вызов сделан с: " + request + " этажа");
+
+        distMin = map.entrySet()
+                .stream()
+                .mapToInt(x -> Math.abs(x.getValue() - request))
+                .min()
+                .getAsInt();
+
+        if (distMin == 0) {
+            Map.Entry<String, Integer> elevActive = map.entrySet()
+                    .stream()
+                    .filter(x -> x.getValue() == request)
+                    .findFirst()
+                    .get();
+            System.out.println("На этаже " + request + " стоит лифт " + elevActive.getKey());
+
+        } else {
+            Map.Entry<String, Integer> elevActive = map.entrySet()
+                    .stream()
+                    .filter(x -> x.getValue() == (request - distMin)
+                            || x.getValue() == (request + distMin))
+                    .findFirst()
+                    .get();
+
+            System.out.println("На вызов с " + request + " этажа едет лифт " + elevActive.getKey()
+                    + " с " + elevActive.getValue() + " этажа");
         }
     }
 }
